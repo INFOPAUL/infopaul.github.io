447e21e8a21c0cfb77f98c21eecd57a46fcb209f
Paul <paul@gmail.com>
2018-12-06 21:34:03 +0200

    no message

---
+++ scripts/plotCreator.js
@@ -0,0 +1,82 @@
+function drawChart() {
+ // Set the dimensions of the canvas / graph
+var margin = {top: 30, right: 20, bottom: 30, left: 50},
+    width = 600 - margin.left - margin.right,
+    height = 270 - margin.top - margin.bottom;
+
+
+// Set the ranges
+var x = d3.scaleTime().range([0, width]);
+var y = d3.scaleLinear().range([height, 0]);
+
+// Define the axes
+var xAxis = d3.axisBottom(x).ticks(5);
+
+var yAxis = d3.axisLeft(y).ticks(5);
+
+// Define the line
+var valueline = d3.line()
+    .x(function(d) { return x(d.key); })
+    .y(function(d) { return y(d.value); });
+    
+// Adds the svg canvas
+var svg = d3.select("body")
+    .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+        .attr("transform", 
+              "translate(" + margin.left + "," + margin.top + ")");
+
+// Get the data
+d3.json("geo/data/UNHCR_Statistic.json", function(error, data) {
+    /*data.forEach(function(d) {
+        d.date = parseDate(d.date);
+        d.close = +d.close;
+    });*/
+
+    var data = d3.nest()
+				 .key(function(d) { 
+				 	return d.year;
+				 })
+				 .rollup(function(d) { 
+					return d3.sum(d, function(g) {
+							return g.ref; 
+						});
+				 })
+				 .entries(data);
+
+
+
+    // Scale the range of the data
+    x.domain(d3.extent(data, function(d) { return d.key; }));
+    y.domain([0, d3.max(data, function(d) { return d.value; })]);
+
+    // Add the valueline path.
+    svg.append("path")
+        .attr("class", "line")
+        .attr("d", valueline(data));
+
+    // Add the scatterplot
+    svg.selectAll("dot")
+        .data(data)
+      .enter().append("circle")
+        .attr("r", 3.5)
+        .attr("cx", function(d) { return x(d.date); })
+        .attr("cy", function(d) { return y(d.close); });
+
+    // Add the X Axis
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    // Add the Y Axis
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
+
+	});
+
+}
+drawChart();
