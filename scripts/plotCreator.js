10711fd43478b4aaf7709478451ab91d80b0695d
Paul <paul@gmail.com>
2018-12-13 15:41:38 +0200

    no message

--- scripts/plotCreator.js
+++ scripts/plotCreator.js
@@ -153,7 +153,7 @@ var svg = d3.select("#" + id)
         .attr("height", height + margin.top + margin.bottom)
     .append("g")
         .attr("transform", 
-              "translate(" + margin.left + "," + margin.top + ")");
+              "translate(" + margin.left + "," + 0 + ")");
 
 // Get the data
 d3.json(url)
