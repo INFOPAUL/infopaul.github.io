ecbbd3c3a94066bed0d9970cca434212922b7172
Paul <paul@gmail.com>
2018-12-13 14:44:12 +0200

    no message

--- scripts/plotCreator.js
+++ scripts/plotCreator.js
@@ -1,4 +1,4 @@
-function drawChart() {
+function drawChart(id, url) {
  // Set the dimensions of the canvas / graph
 var margin = {top: 30, right: 20, bottom: 30, left: 70},
     width = 500 - margin.left - margin.right,
@@ -9,7 +9,7 @@ var margin = {top: 30, right: 20, bottom: 30, left: 70},
 		var y = d3.scaleLinear().range([height, 0]);
     
 // Adds the svg canvas
-var svg = d3.select("#UNRefugeePlot")
+var svg = d3.select("#" + id)
     .append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
@@ -18,7 +18,7 @@ var svg = d3.select("#UNRefugeePlot")
               "translate(" + margin.left + "," + margin.top + ")");
 
 // Get the data
-d3.json("scripts/data/UNHCR_Statistic.json")
+d3.json(url)
   .then(function(data){
        data = d3.nest()
   				 .key(function(d) { 
@@ -129,4 +129,126 @@ d3.json("scripts/data/UNHCR_Statistic.json")
   });
 
 }
-drawChart();
+drawChart("UNRefugeePlot", "scripts/data/UNHCR_Statistic.json");
+
+
+
+function drawTonesChart(id, url) {
+ // Set the dimensions of the canvas / graph
+var margin = {top: 30, right: 20, bottom: 30, left: 70},
+    width = 500 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+
+		var x = d3.scaleLinear().range([0, width]);
+		var y = d3.scaleLinear().range([height, 0]);
+    
+// Adds the svg canvas
+var svg = d3.select("#" + id)
+    .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+        .attr("transform", 
+              "translate(" + margin.left + "," + margin.top + ")");
+
+// Get the data
+d3.json(url)
+  .then(function(data){      
+ 
+  	 // Set the ranges
+		var maxKey = d3.max(data, function(d) { return d.year_date;} );
+		var maxValue = d3.max(data, function(d) { return d.median_tone;} );
+		var minKey = d3.min(data, function(d) { return d.year_date;} );
+		var minValue = d3.min(data, function(d) { return d.median_tone;} );
+
+
+
+
+		// Define the line
+		var valueline = d3.line()
+   		 .x(function(d) { 
+    		return x(d.key); 
+    	})
+    		.y(function(d) { 
+    		return y(d.value); 
+    	})
+        .curve(d3.curveMonotoneX);
+      // Scale the range of the data
+      x.domain([minKey, maxKey]);
+      y.domain([minValue, maxValue]);	
+      // Add the valueline path.
+      svg.append("path")
+          .data([data])
+          .attr("class", "line")
+          .attr("d", valueline);	
+      // Add the scatterplot
+      /*svg.selectAll("dot")
+          .data(data)
+        .enter().append("circle")
+          .attr("r", 3.5)
+          .attr("cx", function(d) { return x(d.date); })
+          .attr("cy", function(d) { return y(d.close); });
+  */
+      // Add the X Axis
+      svg.append("g")
+          .attr("transform", "translate(0," + height + ")")
+          .call(d3.axisBottom(x).tickFormat(function(d){
+          	return Math.floor(d / 10000);
+          }));	
+      // Add the Y Axis
+      svg.append("g")
+          .call(d3.axisLeft(y).tickFormat(function(d){
+          	return d;
+          }));
+
+         // Add the tooltip container to the vis container
+              // it's invisible and its position/contents are defined during mouseover
+
+
+		 
+
+              // tooltip mouseover event handler
+              var tipMouseover = function(d) {
+
+				var xPosition = d3.event.pageX;
+    		var yPosition = d3.event.pageY;
+
+    				var date = d3.time.format("%Y%m%d");
+                  var html  = "Date: " + d.year_date + "<br/>" +
+                              "Value: " + d.median_tone;
+
+                   d3.select("#tooltip")
+					.classed("hidden",false)
+					.html(html)
+                      .style("left", (xPosition + 15) + "px")
+                      .style("top", (yPosition - 15) + "px")
+
+              };
+              // tooltip mouseout event handler
+              var tipMouseout = function(d) {
+                  d3.select("#tooltip")
+                  .classed("hidden", true)
+              };
+
+
+
+
+
+      svg.selectAll(".dot")
+    .data(data)
+  .enter().append("circle") // Uses the enter().append() method
+    .attr("class", "dot") // Assign a class for styling
+    .attr("cx", function(d, i) { return x(d.key) })
+    .attr("cy", function(d) { return y(d.value) })
+    .attr("r", 5)
+    .on("mouseover", tipMouseover)
+    .on("mouseout", tipMouseout);
+  	})	
+  .catch(function(error) {
+  	console.log(error);
+  });
+
+}
+
+drawTonesChart("tonesEveryDayPlot", "scripts/data/tones_counts_2015.json")
